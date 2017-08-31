0f6ee55fa74bd3a747e9695186ac0e84274c2bbd
hyuni106 <suhyuni106@hanmail.net>
2017-08-31 10:24:24 +0900

    페이스북 로그인

--- app/src/main/java/kr/co/tjeit/lecturemanager/LoginActivity.java
+++ app/src/main/java/kr/co/tjeit/lecturemanager/LoginActivity.java
@@ -1,26 +1,36 @@
 package kr.co.tjeit.lecturemanager;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginResult;
+import com.kakao.auth.Session;
+import com.kakao.usermgmt.LoginButton;
+
+public class LoginActivity extends BaseActivity {
 
     private Button signUpBtn;
     private Button loginBtn;
 
     public static LoginActivity myActivity;
+    private com.facebook.login.widget.LoginButton fbLoginBtn;
+    private com.kakao.usermgmt.LoginButton comkakaologin;
+
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         myActivity = this;
-
-        signUpBtn = (Button) findViewById(R.id.signUpBtn);
-        loginBtn = (Button) findViewById(R.id.loginBtn);
 
         signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,5 +49,58 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public void setUpEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+        callbackManager = CallbackManager.Factory.create();
+        fbLoginBtn.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+        ProfileTracker pt = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
+                if (currentProfile == null) {
+                    Toast.makeText(mContext, "로그아웃 처리 되었습니다.", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(mContext, currentProfile.getName() + "님 접속", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(mContext, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void bindViews() {
+        this.comkakaologin = (LoginButton) findViewById(R.id.com_kakao_login);
+        this.fbLoginBtn = (com.facebook.login.widget.LoginButton) findViewById(R.id.fbLoginBtn);
+        signUpBtn = (Button) findViewById(R.id.signUpBtn);
+        loginBtn = (Button) findViewById(R.id.loginBtn);
     }
 }
