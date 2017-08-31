c374466fd532440d0428fb575478606fb446edd7
cho881020 <cho881020@gmail.com>
2017-08-31 12:18:02 +0900

    카카오톡/페이스북 로그인 버튼 완료

--- app/src/main/java/kr/co/tjeit/lecturemanager/LoginActivity.java
+++ app/src/main/java/kr/co/tjeit/lecturemanager/LoginActivity.java
@@ -1,26 +1,51 @@
 package kr.co.tjeit.lecturemanager;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.kakao.auth.ISessionCallback;
+import com.kakao.auth.Session;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.LoginButton;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.MeResponseCallback;
+import com.kakao.usermgmt.response.model.UserProfile;
+import com.kakao.util.exception.KakaoException;
+
+public class LoginActivity extends BaseActivity {
 
     private Button signUpBtn;
     private Button loginBtn;
-
     public static LoginActivity myActivity;
+    KakaoSessionCallback ksc;
+
+    CallbackManager cm;
+    ProfileTracker pt;
+    private com.facebook.login.widget.LoginButton fbLoginBtn;
+    private com.kakao.usermgmt.LoginButton comkakaologin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         myActivity = this;
+        bindViews();
+        setupEvents();
+        setValues();
+    }
 
-        signUpBtn = (Button) findViewById(R.id.signUpBtn);
-        loginBtn = (Button) findViewById(R.id.loginBtn);
+    @Override
+    public void setupEvents() {
 
         signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,6 +63,97 @@ public class LoginActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    public void setValues() {
+
+//        화면이 시작되면 무조건 로그아웃 처리
+//        강의의 편의를 위해 작성하는 코드. (실제로는 안짬)
+
+//        페북 로그아웃
+        LoginManager.getInstance().logOut();
+//        카톡 로그아웃
+        UserManagement.requestLogout(null);
+
+        ksc = new KakaoSessionCallback();
+        Session.getCurrentSession().addCallback(ksc);
+
+        pt = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
+                if (currentProfile == null) {
+//                    로그아웃 됨.
+                }
+                else {
+//                    로그인 됨.
+                    Toast.makeText(mContext, currentProfile.getName() + "님 접속", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+        cm = CallbackManager.Factory.create();
+        fbLoginBtn.registerCallback(cm, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        cm.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void bindViews() {
+
+        this.signUpBtn = (Button) findViewById(R.id.signUpBtn);
+        this.comkakaologin = (LoginButton) findViewById(R.id.com_kakao_login);
+        this.fbLoginBtn = (com.facebook.login.widget.LoginButton) findViewById(R.id.fbLoginBtn);
+        this.loginBtn = (Button) findViewById(R.id.loginBtn);
+    }
+
+    private class KakaoSessionCallback implements ISessionCallback {
+
+        @Override
+        public void onSessionOpened() {
+
+            UserManagement.requestMe(new MeResponseCallback() {
+                @Override
+                public void onSessionClosed(ErrorResult errorResult) {
+
+                }
+
+                @Override
+                public void onNotSignedUp() {
+
+                }
+
+                @Override
+                public void onSuccess(UserProfile result) {
+                    Toast.makeText(LoginActivity.this, result.getNickname()+"님 접속", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        @Override
+        public void onSessionOpenFailed(KakaoException exception) {
+
+        }
+    }
+
 }
--- app/src/main/java/kr/co/tjeit/lecturemanager/SplashActivity.java
+++ app/src/main/java/kr/co/tjeit/lecturemanager/SplashActivity.java
@@ -1,9 +1,17 @@
 package kr.co.tjeit.lecturemanager;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -20,5 +28,21 @@ public class SplashActivity extends AppCompatActivity {
 
             }
         }, 2000);
+
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "kr.co.tjeit.lecturemanager",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
     }
 }
