1900018e634bb6de9b8f3d63733e26d95a0b7dd6
cho881020 <cho881020@gmail.com>
2017-09-04 10:16:46 +0900

    서버 통신 준비 작업

--- app/src/main/java/kr/co/tjeit/lecturemanager/MyProfileActivity.java
+++ app/src/main/java/kr/co/tjeit/lecturemanager/MyProfileActivity.java
@@ -1,5 +1,7 @@
 package kr.co.tjeit.lecturemanager;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,6 +13,7 @@ import com.facebook.AccessToken;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -21,6 +24,7 @@ public class MyProfileActivity extends BaseActivity {
     private de.hdodenhof.circleimageview.CircleImageView profileImg;
     private android.widget.TextView nameTxt;
     private android.widget.Button linkBtn;
+    private TextView genderTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +49,7 @@ public class MyProfileActivity extends BaseActivity {
                 AccessToken.getCurrentAccessToken(),
                 new GraphRequest.GraphJSONObjectCallback() {
                     @Override
-                    public void onCompleted(JSONObject object, GraphResponse response) {
+                    public void onCompleted(final JSONObject object, GraphResponse response) {
 
                         Log.d("사용자정보", object.toString());
 
@@ -56,10 +60,42 @@ public class MyProfileActivity extends BaseActivity {
 
 //                       이 사람의 페이지로 방문할 수 있도록 설정.
 //                                Intent 기능 활용
+
+                                Intent intent = new Intent();
+                                intent.setAction(Intent.ACTION_VIEW);
+                                try {
+                                    String link = object.getString("link");
+
+                                    intent.setData(Uri.parse(link));
+                                    startActivity(intent);
+
+
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+
                             }
                         });
 
 //                        성별 표시 기능.
+
+
+                        String gender = null;
+                        try {
+                            gender = object.getString("gender");
+
+                            if (gender.equals("male")) {
+                                genderTxt.setText("남성");
+                            }
+                            else {
+                                genderTxt.setText("여성");
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
 
                     }
                 }
@@ -78,6 +114,7 @@ public class MyProfileActivity extends BaseActivity {
     @Override
     public void bindViews() {
         this.linkBtn = (Button) findViewById(R.id.linkBtn);
+        this.genderTxt = (TextView) findViewById(R.id.genderTxt);
         this.nameTxt = (TextView) findViewById(R.id.nameTxt);
         this.profileImg = (CircleImageView) findViewById(R.id.profileImg);
     }
