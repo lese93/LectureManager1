bf1bfd62a3d3f4705ed1b6b76725a6fd6d9e680b
cho881020 <cho881020@gmail.com>
2017-09-08 12:57:47 +0900

    쪽지 전송 기능 마무리

--- app/src/main/java/kr/co/tjeit/lecturemanager/SendMessageActivity.java
+++ app/src/main/java/kr/co/tjeit/lecturemanager/SendMessageActivity.java
@@ -1,11 +1,16 @@
 package kr.co.tjeit.lecturemanager;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONObject;
 
 import kr.co.tjeit.lecturemanager.data.User;
+import kr.co.tjeit.lecturemanager.util.ServerUtil;
 
 public class SendMessageActivity extends BaseActivity {
 
@@ -26,7 +31,21 @@ public class SendMessageActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
-
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ServerUtil.send_message(mContext,
+                        contentEdt.getText().toString(),
+                        mUser.getId(),
+                        new ServerUtil.JsonResponseHandler() {
+                            @Override
+                            public void onResponse(JSONObject json) {
+                                contentEdt.setText("");
+                                Toast.makeText(mContext, "쪽지 전송이 완료되었습니다.", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
     }
 
     @Override
